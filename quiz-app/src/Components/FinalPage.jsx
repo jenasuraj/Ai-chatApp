6c42fbb6ff5efdf3b1082d00503296888e46d15e
jenasuraj <jenasuraj742@gmail.com>
2024-10-18 13:32:41 +0530

    Removed all project folders except 'Google_gemini'

--- quiz-app/src/Components/FinalPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FinalPage = ({score}) => {
-  return (
-  <h1>Your total marks is : {score}</h1>
-  )
-}
-
-export default FinalPage
--- todo-list/src/store/todo-items-store.jsx
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const TodoItemsContext = createContext([]); //todoitems is iin capotyal cuse it is gonna be export to amny and is gonna be used as compo
